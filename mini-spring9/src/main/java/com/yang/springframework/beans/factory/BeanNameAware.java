137210c89357f3bffe8da90b400ea011f1601f68
Yang_z <yangwz9527@foxmail.com>
2026-03-26 16:52:42 +0800

    给Aware添加注释

--- mini-spring9/src/main/java/com/yang/springframework/beans/factory/BeanNameAware.java
+++ mini-spring9/src/main/java/com/yang/springframework/beans/factory/BeanNameAware.java
@@ -1,5 +1,18 @@
 package com.yang.springframework.beans.factory;
 
+/**
+ * 由希望感知其在 bean 工厂中 bean 名称的 bean 实现的接口。需要注意的是，通常不建议对象依赖其 bean 名称，
+ * 因为这种依赖既可能对外部配置敏感，也可能是对 Spring API 的不必要依赖。
+ * <p>有关所有 bean 生命周期方法的列表，请参见 BeanFactory 的 Javadoc。
+ */
 public interface BeanNameAware extends Aware {
+
+    /**
+     * 设置创建此bean的bean工厂中的bean名称。
+     * <p>在普通bean属性填充完成后、初始化回调（如InitializingBean.afterPropertiesSet()或自定义的init-method）之前调用。
+     * @param name bean在工厂中的名称。请注意，此名称是工厂中实际使用的bean名称，可能与最初指定的名称不同：特别是对于内部bean，
+     *             实际bean名称可能通过添加"#..."后缀进行了唯一化处理。如果需要，可以使用BeanFactoryUtils.originalBeanName(String)
+     *             方法来提取原始bean名称（不带后缀）。
+     */
     void setBeanName(String name);
 }
--- mini-spring9/src/main/java/com/yang/springframework/context/ApplicationContextAware.java
+++ mini-spring9/src/main/java/com/yang/springframework/context/ApplicationContextAware.java
@@ -3,6 +3,25 @@ package com.yang.springframework.context;
 import com.yang.springframework.beans.BeansException;
 import com.yang.springframework.beans.factory.Aware;
 
+/**
+ * 由任何希望获知其运行所在的ApplicationContext的对象所实现的接口。
+ * <p>例如，当一个对象需要访问一组协作的bean时，实现此接口是有意义的。需要注意的是，通过bean引用进行配置优于仅为bean查找目的而实现此接口。
+ * <p>如果对象需要访问文件资源（即希望调用getResource）、希望发布应用事件或需要访问MessageSource，也可以实现此接口。但在这种特定场景下，
+ * 更推荐实现更具体的ResourceLoaderAware、ApplicationEventPublisherAware或MessageSourceAware接口。
+ * <p>请注意，文件资源依赖也可以通过类型为org.springframework.core.io.Resource的bean属性暴露，并通过bean工厂利用字符串自动类型转换进行填充。
+ * 这消除了仅为访问特定文件资源而实现任何回调接口的必要性。
+ * <p>org.springframework.context.support.ApplicationObjectSupport是一个为应用对象提供的便捷基类，实现了此接口。
+ * <p>有关所有bean生命周期方法的列表，请参见BeanFactory的javadoc。
+ */
 public interface ApplicationContextAware extends Aware {
+
+    /**
+     * 设置此对象运行所在的 ApplicationContext。通常，此调用将用于初始化对象。
+     * <p>在普通 bean 属性填充之后、但在初始化回调（如 org.springframework.beans.factory.InitializingBean.afterPropertiesSet()
+     * 或自定义 init-method）之前调用。如果适用，将在 ResourceLoaderAware.setResourceLoader、
+     * ApplicationEventPublisherAware.setApplicationEventPublisher 和 MessageSourceAware 之后调用。
+     * @param applicationContext 此对象将要使用的 ApplicationContext 对象
+     * @throws BeansException 如果 application context 方法抛出异常
+     */
     void setApplicationContext(ApplicationContext applicationContext) throws BeansException;
 }
